8b4a6649deeb18f1edebcfa64fd983baea9eab2b
Amanda Trigueiro <atrigueiro@atlassian.com>
2021-09-06 15:11:25 +1000

    add new file

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-const StringLiteral = createToken( { name: 'StringLiteral', pattern: /"(:?[^\\"\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/
-console.log('test');
+const xssFilters = require('xss-filters'); 
+const util = require('util');  
+const app = require('http');
+app.get('/', (req, res) => {   const unsafeFirstname = req.query.firstname;   
+// const safeFirstname = xssFilters.inHTMLData(unsafeFirstname);    
+res.send(util.format('<h1>Tom%s</h1>', unsafeFirstname)); });  
+
+app.listen(3000);
